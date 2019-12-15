6a1417a3a9935955f7cb94b78846e4233501b50e
Clement Kofi Nartey <cak.nartey@gmail.com>
2019-12-16 00:18:45 +0100

    clean up 1

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,4 @@
-import 'package:clima/screens/city_screen.dart';
-import 'package:clima/screens/location_screen.dart';
 import 'package:flutter/material.dart';
-import 'package:clima/screens/loading_screen.dart';
 
 void main() => runApp(MyApp());
 
